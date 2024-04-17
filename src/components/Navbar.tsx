af61073bce4951ae0ea1ebd2568efefad1e1981e
jivjava2203 <jivjava2203@gmail.com>
2024-04-17 09:29:42 +0800

    added tab and navbar logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,7 @@ interface Props {
 export default function Navbar({ className }: Props) {
   return (
     <div className={`navbar bg-base-100 border-b ${className}`}>
+      <img src="logo.png" alt="Logo" className="navbar-logo" />
       <a className="btn btn-ghost text-md sm:text-lg">LOOK Disk Scheduling Visualizer</a>
     </div>
   );
